d1c4e595a9eb85f48ae4c81defb76cbfa70df5c2
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2023-09-21 18:52:39 -0400

    max speed from 2.4 to 3.6, switch to `WPI_Pigeon2`, add button for zeroing heading in disabled

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 2.4;
+    public static final double kMaxSpeedMetersPerSecond = 3.6;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
@@ -63,6 +63,7 @@ public final class Constants {
     public static final int kRearRightTurningCanId = 16;
 
     public static final boolean kGyroReversed = false;
+    public static final int kGyroCanId = 1;
   }
 
   public static final class ModuleConstants {
