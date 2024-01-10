bd908be5ca67cdee1692386b298616b8c81dc343
Lewis Arias <lewisgarias@outlook.com>
2024-01-09 21:05:22 -0500

    Add gversion for recording build constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,6 +37,24 @@ public class Robot extends LoggedRobot {
      */
     @Override
     public void robotInit() {
+        // Record metadata
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        switch (BuildConstants.DIRTY) {
+            case 0:
+                Logger.recordMetadata("GitDirty", "All changes committed");
+                break;
+            case 1:
+                Logger.recordMetadata("GitDirty", "Uncomitted changes");
+                break;
+            default:
+                Logger.recordMetadata("GitDirty", "Unknown");
+                break;
+        }
+
         switch (AdvantageKitConstants.getMode()) {
             case REAL:
                 // Running on a real robot, log to a file
