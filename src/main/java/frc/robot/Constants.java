21925fb652ceaace90f4648ca7d8979315d59a5e
Lewis Arias <lewisgarias@outlook.com>
2024-01-09 19:49:20 -0500

    Remove `AutoConstants` as they're unused

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -143,21 +143,6 @@ public final class Constants {
         public static final double kDriveDeadband = 0.05;
     }
 
-    public static final class AutoConstants {
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-        public static final double kPXController = 1;
-        public static final double kPYController = 1;
-        public static final double kPThetaController = 1;
-
-        // Constraint for the motion profiled robot angle controller
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
-                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-    }
-
     public static final class NeoMotorConstants {
         public static final double kFreeSpeedRpm = 5676;
     }
