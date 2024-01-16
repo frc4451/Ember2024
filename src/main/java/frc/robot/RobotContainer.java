388a98d211949977d2ef14b0396a4c5915b99982
Judson James <judsonjames96@gmail.com>
2024-01-16 02:44:16 -0500

    Make state machine dependent trigger use Deferred commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
@@ -114,7 +115,10 @@ public class RobotContainer {
                         () -> m_robotDrive.setCross(),
                         m_robotDrive));
 
-        m_driverController.povUp().whileTrue(m_pathChooser.get());
+        m_driverController.povUp()
+                .whileTrue(
+                        Commands.deferredProxy(
+                                () -> m_pathChooser.get()));
 
         // Make a SmartDashboard button
         SmartDashboard.putData("Run Test Path", PathPlannerUtils.getTestPath());
