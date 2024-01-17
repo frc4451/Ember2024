13ae3ccf7423612ff0d9d963e4f38dd0b0d0c6ec
Judson James <judsonjames96@gmail.com>
2024-01-17 18:56:34 -0500

    Format `VirtualSubsystem`

--- src/main/java/frc/utils/VirtualSubsystem.java
+++ src/main/java/frc/utils/VirtualSubsystem.java
@@ -11,44 +11,44 @@ import java.util.List;
  * Examples could be: - VisionSubsystem - PathPlannerSubsystem
  */
 public abstract class VirtualSubsystem {
-  private static List<VirtualSubsystem> virtualSubsystems = new ArrayList<>();
+    private static List<VirtualSubsystem> virtualSubsystems = new ArrayList<>();
 
-  /**
-   * Every subsystem that extends VirtualSubsystem gets added to the list of
-   * Subsystems that are
-   * called every robot period.
-   */
-  public VirtualSubsystem() {
-    virtualSubsystems.add(this);
-  }
-
-  public static void listVirtualSubsystems() {
-    for (VirtualSubsystem subsystem : virtualSubsystems) {
-      String message = String.format(
-          "[robotzgarage] Connected Virtual Subsystem '%s'",
-          subsystem.getClass().getSimpleName());
-      System.out.println(message);
+    /**
+     * Every subsystem that extends VirtualSubsystem gets added to the list of
+     * Subsystems that are
+     * called every robot period.
+     */
+    public VirtualSubsystem() {
+        virtualSubsystems.add(this);
     }
-  }
 
-  /** Call {@link #periodic()} on every virtual subsystem. */
-  public static void runPeriodically() {
-    for (VirtualSubsystem subsystem : virtualSubsystems) {
-      subsystem.periodic();
+    public static void listVirtualSubsystems() {
+        for (VirtualSubsystem subsystem : virtualSubsystems) {
+            String message = String.format(
+                    "[robotzgarage] Connected Virtual Subsystem '%s'",
+                    subsystem.getClass().getSimpleName());
+            System.out.println(message);
+        }
     }
-  }
 
-  /** Call {@link #simulationPeriodic()} on every virtual subsystem. */
-  public static void runSimulationPeriodically() {
-    for (VirtualSubsystem subsystem : virtualSubsystems) {
-      subsystem.simulationPeriodic();
+    /** Call {@link #periodic()} on every virtual subsystem. */
+    public static void runPeriodically() {
+        for (VirtualSubsystem subsystem : virtualSubsystems) {
+            subsystem.periodic();
+        }
     }
-  }
 
-  /** Method to run on every thread update. */
-  public abstract void periodic();
+    /** Call {@link #simulationPeriodic()} on every virtual subsystem. */
+    public static void runSimulationPeriodically() {
+        for (VirtualSubsystem subsystem : virtualSubsystems) {
+            subsystem.simulationPeriodic();
+        }
+    }
 
-  /** Method to run on every simulated thread update. */
-  public abstract void simulationPeriodic();
+    /** Method to run on every thread update. */
+    public abstract void periodic();
+
+    /** Method to run on every simulated thread update. */
+    public abstract void simulationPeriodic();
 
 }
