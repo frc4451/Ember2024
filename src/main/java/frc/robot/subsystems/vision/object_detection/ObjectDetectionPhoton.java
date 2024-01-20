ddd328b77de64cfd34fff010e09ca63c515329b2
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-01-20 14:01:04 -0500

    ObjectDetection Threading

--- src/main/java/frc/robot/subsystems/vision/object_detection/ObjectDetectionPhoton.java
+++ src/main/java/frc/robot/subsystems/vision/object_detection/ObjectDetectionPhoton.java
@@ -8,14 +8,33 @@ import frc.robot.VisionConstants.VisionSource;
 public class ObjectDetectionPhoton implements ObjectDetectionIO {
     private final PhotonCamera camera;
 
+    private final Thread periodicThread = new Thread(() -> {
+        while (!Thread.currentThread().isInterrupted()) {
+            periodic();
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    });
+
     public ObjectDetectionPhoton(VisionSource source) {
         camera = new PhotonCamera(source.name());
+
+        periodicThread.setPriority(Thread.MAX_PRIORITY);
+        periodicThread.start();
+    }
+
+    private PhotonPipelineResult frame = new PhotonPipelineResult();
+
+    private void periodic() {
+        frame = camera.getLatestResult();
     }
 
     @Override
     public void updateInputs(ObjectDetectionIOInputs inputs) {
-        PhotonPipelineResult frame = camera.getLatestResult();
         inputs.frame = frame;
-
     }
 }
