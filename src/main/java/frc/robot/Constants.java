0a0f95c2915852d60a2d5c036a4c026d7f560aa3
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-01-31 16:44:49 -0500

    change gear ratio back to 240.0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -162,7 +162,7 @@ public final class Constants {
         public static final double kPivotMinDegrees = 10.0;
         public static final double kPivotMaxDegrees = 167.25;
 
-        public static final double kPivotReduction = 80.0;
+        public static final double kPivotReduction = 240.0; // increased from 80.0 back to 240.0
 
         public static final int kPivotCanId = 4;
         public static final double kPivotVelocityRadiansPerSecond = Units.degreesToRadians(15.0);
