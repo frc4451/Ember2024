5a241bbac90f0678f537f3d74f49fbc4d30b998f
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-02-12 16:43:00 -0500

    Remove unused import from `ShooterIOTalonFX`

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -4,7 +4,6 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
