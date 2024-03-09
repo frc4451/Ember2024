8289afb311366e3a38d7d822eb90c26a0fe24d60
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-03-08 19:38:20 -0500

    Remove TODO's

--- src/main/java/frc/robot/subsystems/pivot/PivotLocation.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotLocation.java
@@ -9,8 +9,8 @@ public enum PivotLocation {
     kSoftMin(Rotation2d.fromDegrees(28.0)),
     kElevatorDownSoftMax(Rotation2d.fromDegrees(40.0)),
     kElevatorDownHardMax(Rotation2d.fromDegrees(42.0)),
-    kAmpScoringPosition(Rotation2d.fromDegrees(82.0)), // TODO: fill this in later
-    kTrapScoringPosition(Rotation2d.fromDegrees(82.0)), // TODO: fill this in later
+    kAmpScoringPosition(Rotation2d.fromDegrees(82.0)),
+    kTrapScoringPosition(Rotation2d.fromDegrees(82.0)),
     kElevatorUpSoftMax(Rotation2d.fromDegrees(82.0)),
     ;
 
