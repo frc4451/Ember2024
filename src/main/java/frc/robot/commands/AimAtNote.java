5e47c666f98601c76acf4110bf9b479d012711a7
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-03-18 18:59:30 -0400

    Fix note aiming becoming field-relative when the button is held but a note isn't need & fix stage lane assist having the wrong rotation.

--- src/main/java/frc/robot/commands/AimAtNote.java
+++ src/main/java/frc/robot/commands/AimAtNote.java
@@ -66,7 +66,7 @@ public class AimAtNote extends Command {
                     ySupplier.getAsDouble(),
                     thetaSupplier.getAsDouble(),
                     false,
-                    true);
+                    false);
             return;
         }
 
