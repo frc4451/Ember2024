56075ad918d81d5beb9f3f0e60c1b2abfcecda4f
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-03-22 12:18:01 -0400

    Speed up manual feeding shot.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -221,7 +221,7 @@ public class RobotContainer {
                 .onFalse(m_shooter.stopCommand());
         m_operatorController.povDown()
                 .onTrue(
-                        m_shooter.setVelocityShooterCommand(47.8, 47.8)
+                        m_shooter.setVelocityShooterCommand(54.9, 54.9)
                                 .alongWith(m_pivot.setGoalCommand(Rotation2d.fromDegrees(42))))
                 .onFalse(m_shooter.stopCommand());
         // up against the subwoofer
