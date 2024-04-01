ba0955647a632c183f12ae3d59efc4cf912fc870
Driver Station <34461450+4451-Driver-Station@users.noreply.github.com>
2024-04-01 19:37:11 -0400

    BlinkinState changes

--- src/main/java/frc/robot/subsystems/blinkin/BlinkinState.java
+++ src/main/java/frc/robot/subsystems/blinkin/BlinkinState.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems.blinkin;
 public enum BlinkinState {
     // Priority is determined by definition order,
     // e.g. NOTE has precedence over DEFAULT
-    NOTE(BlinkinColors.SOLID_ORANGE, BlinkinPattern.BLINK),
-    DEFAULT(BlinkinColors.SOLID_BLACK, BlinkinPattern.SOLID);
+    NOTE(BlinkinColors.SOLID_BLUE, BlinkinPattern.BLINK),
+    DEFAULT(BlinkinColors.SOLID_BLUE, BlinkinPattern.SOLID);
 
     public final BlinkinColors color;
     public final BlinkinPattern pattern;
